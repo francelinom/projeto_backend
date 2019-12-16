b63f9db3736647f19698daf6cd4822c86892f655
Francelino Marcilio <fmarcilio1989@gmail.com>
2019-12-16 17:59:07 -0300

    criando operacao de instanciacao

--- src/main/java/com/ufrn/eaj/demo/ProjetoBackendApplication.java
+++ src/main/java/com/ufrn/eaj/demo/ProjetoBackendApplication.java
@@ -1,13 +1,32 @@
 package com.ufrn.eaj.demo;
 
+import com.ufrn.eaj.demo.domain.Categoria;
+import com.ufrn.eaj.demo.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjetoBackendApplication {
+public class ProjetoBackendApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProjetoBackendApplication.class, args);
     }
+
+    @Override
+    public void run(String... args)throws Exception{
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
+
 
 }
