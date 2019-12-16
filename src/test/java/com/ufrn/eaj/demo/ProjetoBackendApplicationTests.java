a4640a0c562b61f9bac6d7942eabb499ead0628b
francelino <fmarcilio1989@gmail.com>
2019-12-16 13:33:39 -0300

    projeto criado

---
+++ src/test/java/com/ufrn/eaj/demo/ProjetoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.eaj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
