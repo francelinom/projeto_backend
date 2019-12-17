454503a8c7924562caeb250194a980e499f1b83d
francelino <fmarcilio1989@gmail.com>
2019-12-17 09:39:27 -0300

    produto e associacao muito para muitos

--- src/main/java/com/ufrn/eaj/demo/ProjetoBackendApplication.java
+++ src/main/java/com/ufrn/eaj/demo/ProjetoBackendApplication.java
@@ -1,7 +1,9 @@
 package com.ufrn.eaj.demo;
 
 import com.ufrn.eaj.demo.domain.Categoria;
+import com.ufrn.eaj.demo.domain.Produto;
 import com.ufrn.eaj.demo.repositories.CategoriaRepository;
+import com.ufrn.eaj.demo.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class ProjetoBackendApplication implements CommandLineRunner {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
 
         SpringApplication.run(ProjetoBackendApplication.class, args);
@@ -25,7 +30,19 @@ public class ProjetoBackendApplication implements CommandLineRunner {
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
 
+        Produto p1 = new Produto(null, "notebook", (float)2850.00);
+        Produto p2 = new Produto(null, "Impressora", (float)800.00);
+        Produto p3 = new Produto(null, "mouse", (float)80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 
 
