f79c839508e43baefedf6b73d91da4eb49d8083e
francelino <fmarcilio1989@gmail.com>
2019-12-19 10:27:34 -0300

    inserindo uma nova Categoria com Post

--- src/main/java/com/ufrn/eaj/demo/resources/CategoriaResource.java
+++ src/main/java/com/ufrn/eaj/demo/resources/CategoriaResource.java
@@ -4,11 +4,10 @@ import com.ufrn.eaj.demo.domain.Categoria;
 import com.ufrn.eaj.demo.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +23,12 @@ public class CategoriaResource {
 
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
 
-
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
