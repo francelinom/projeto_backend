1cf1fc06a59153d53d05167fc40611da264c385e
Francelino Marcilio <fmarcilio1989@gmail.com>
2019-12-25 11:27:48 -0300

    ajustando delete de cliente

--- src/main/java/com/ufrn/eaj/demo/services/ClienteService.java
+++ src/main/java/com/ufrn/eaj/demo/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
         try {
             repo.deleteById(id);
         } catch (DataIntegrityViolationException e){
-            throw new DataIntegrityException("Não é possível excluir uma Cliente que possui pedidos");
+            throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
         }
     }
 
