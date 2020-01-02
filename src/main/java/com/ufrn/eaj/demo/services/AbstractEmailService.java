d88f030eb6061bcd4d5c957f60a6d3e0947f9c00
francelino <fmarcilio1989@gmail.com>
2020-01-02 13:23:26 -0300

    implementando email HTML

--- src/main/java/com/ufrn/eaj/demo/services/AbstractEmailService.java
+++ src/main/java/com/ufrn/eaj/demo/services/AbstractEmailService.java
@@ -1,23 +1,36 @@
 package com.ufrn.eaj.demo.services;
 
 import com.ufrn.eaj.demo.domain.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
-public abstract class AbstractEmailService implements EmailService{
+public abstract class AbstractEmailService implements EmailService {
 
     @Value("${default.sender}")
     private String sender;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
-    public void sendOrderConfirmationEmail(Pedido obj){
+    public void sendOrderConfirmationEmail(Pedido obj) {
         SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
         sendEmail(sm);
     }
 
-    protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj){
+    protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
         SimpleMailMessage sm = new SimpleMailMessage();
         sm.setTo(obj.getCliente().getEmail());
         sm.setFrom(sender);
@@ -25,6 +38,33 @@ public abstract class AbstractEmailService implements EmailService{
         sm.setSentDate(new Date((System.currentTimeMillis())));
         sm.setText(obj.toString());
         return sm;
+    }
+
+    protected String htmlFromTemplatePedido(Pedido obj) {
+        Context context = new Context();
+        context.setVariable("pedido", obj);
+        return templateEngine.process("email/confirmacaoPedido", context);
+    }
+
+    @Override
+    public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+        try {
+            MimeMessage mm = prepareMimeMessageFromPedido(obj);
+            sendHtmlEmail(mm);
+        } catch (MessagingException e) {
+            sendOrderConfirmationEmail(obj);
+        }
+    }
+
+    protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+        mmh.setTo(obj.getCliente().getEmail());
+        mmh.setFrom(sender);
+        mmh.setSubject("Pedido confirmado! Codigo: " + obj.getId());
+        mmh.setSentDate(new Date(System.currentTimeMillis()));
+        mmh.setText(htmlFromTemplatePedido(obj), true);
+        return mimeMessage;
     }
 
 }
