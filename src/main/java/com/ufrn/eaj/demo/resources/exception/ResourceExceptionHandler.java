e62b70fb9c161406f3b72efd5b5072425c7ab640
Francelino <fmarcilio1989@gmail.com>
2020-01-30 16:11:48 -0300

    tratando excecoes adequadamente

--- src/main/java/com/ufrn/eaj/demo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/ufrn/eaj/demo/resources/exception/ResourceExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.ufrn.eaj.demo.resources.exception;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.ufrn.eaj.demo.services.exceptions.AuthorizationException;
 import com.ufrn.eaj.demo.services.exceptions.DataIntegrityException;
+import com.ufrn.eaj.demo.services.exceptions.FileException;
 import com.ufrn.eaj.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,5 +48,33 @@ public class ResourceExceptionHandler {
 
         StandarError err = new StandarError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+    }
+
+    @ExceptionHandler(FileException.class)
+    public ResponseEntity<StandarError> file(FileException e, HttpServletRequest request){
+
+        StandarError err = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonServiceException.class)
+    public ResponseEntity<StandarError> amazonService(AmazonServiceException e, HttpServletRequest request){
+        HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+        StandarError err = new StandarError(System.currentTimeMillis(), code.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(code).body(err);
+    }
+
+    @ExceptionHandler(AmazonClientException.class)
+    public ResponseEntity<StandarError> amazonClient(AmazonClientException e, HttpServletRequest request){
+
+        StandarError err = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonS3Exception.class)
+    public ResponseEntity<StandarError> amazonS3(AmazonS3Exception e, HttpServletRequest request){
+
+        StandarError err = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Acesso Negado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
